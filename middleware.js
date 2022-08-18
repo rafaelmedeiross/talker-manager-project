5b1a71f5c9ec5e5a1f476fab8fc34646566187dc
rafaelmedeiross <rafael.medeiros@msn.com>
2022-08-18 10:42:31 -0300

    req2

--- middleware.js
+++ middleware.js
@@ -1,7 +1,14 @@
 const fs = require('fs').promises;
 
+const getTalker = async (require, _response, next) => {
+    const reading = await fs.getTalker('talker.json', 'utf-8');
+    const answer = JSON.parse(reading);
+    req.answer = answer;
+    next();
+  };
+
 const searchPeople = async (request, response) => {
-  const reading = await fs.readFile('talker.json', 'utf-8');
+  const reading = await fs.getTalker('talker.json', 'utf-8');
   const { id } = request.params;
   const talker = JSON.parse(reading).find((person) => person.id === Number(id));
   if (!talker) {
